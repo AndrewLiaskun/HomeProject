29c4648933f13a7b19ab43bc7bfc218d34f218b4
hiiga <hiiga@DESKTOP-FUPGRHS>
2021-12-12 12:34:11 +0200

    Add project files.

---
+++ HomeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
